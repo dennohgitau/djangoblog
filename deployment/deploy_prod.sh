e258194c442d6932c3d18d46434d5fc6131c3539
Denis Gitau <denisgitaustudy@gmail.com>
2024-09-25 17:01:20 +0300

    edited the ssh in deploy_prod

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-
-ssh root@192.168.125.107 <<EOF
-  cd drfblogproject
-  git pull
-  source /opt/envs/drfblogproject/bin/activate
-  pip install -r requirements.txt
-  ./manage.py makemigrations
-  ./manage.py migrate  --run-syncdb
-  sudo service gunicorn restart
-  sudo service nginx restart
-  exit
+ssh denis@192.168.125.107 <<EOF
+  sudo -su root <<EOC
+    cd /root/drfblogproject
+    git pull
+    source /opt/envs/drfblogproject/bin/activate
+    pip install -r requirements.txt
+    ./manage.py makemigrations
+    ./manage.py migrate --run-syncdb
+    sudo service gunicorn restart
+    sudo service nginx restart
+    exit
+  EOC
 EOF
